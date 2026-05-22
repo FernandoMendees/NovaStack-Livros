449c0c57415da094a7f277a8e6f3f881af7e9e8a
fernando <fernandofilho2k22@gmail.com>
2026-05-22 11:13:10 -0300

    refactor: melhoria da navegação entre páginas, estilização e estrutura HTML

---
+++ API/books.js
@@ -0,0 +1,13 @@
+export async function registerBooks(name, author, category, date) {
+    const book = {
+        name,
+        author,
+        category,
+        date
+    };
+
+    try {
+        const response = await fetch();
+    }
+    
+}
