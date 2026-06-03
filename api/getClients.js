937ef05c8e943d87670062f577029cb7c246c249
fernando <fernandofilho2k22@gmail.com>
2026-06-03 19:32:51 -0300

    feat: criar requisição que busca clientes não banidos e select dinâmico

--- api/getClients.js
+++ api/getClients.js
@@ -8,9 +8,7 @@ export async function getClients(token) {
             }
         });
 
-        if (!response.ok) {
-            throw new Error("Erro ao buscar clientes");
-        }
+        if (!response.ok) throw new Error("Erro ao buscar clientes");
 
         const data = await response.json()
 
@@ -19,4 +17,25 @@ export async function getClients(token) {
     } catch(error) {
         throw error;
     }  
+}
+
+export async function clientNotBanned(token) {
+    try {
+        const response = await fetch("http://localhost:8080/client/notBanned", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        if (!response.ok) throw new Error(`Não foi possível buscar os clientes. Status [${response.status}]`);
+
+        const data = await response.json();
+
+        return await data;
+
+    } catch (error) {
+        throw error;
+    }
 }
--- js/selectsLoan.js
+++ js/selectsLoan.js
@@ -1,5 +1,9 @@
 import { itemsNotBorrowed } from "../api/getItens.js";
+import { clientNotBanned } from "../api/getClients.js";
+
 const selectItems = document.getElementById("item-loan");
+const selectClients = document.getElementById("client-loan");
+
 const token = localStorage.getItem("TOKEN")
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -13,4 +17,17 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         selectItems.appendChild(option);
     });
+})
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const client = await clientNotBanned(token);
+
+    client.forEach(clients => {
+        const option = document.createElement("option");
+
+        option.value = clients.id;
+        option.textContent = clients.name;
+
+        selectClients.appendChild(option);
+    })
 })
